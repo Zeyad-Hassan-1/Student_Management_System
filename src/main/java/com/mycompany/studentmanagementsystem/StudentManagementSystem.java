626b63926030eecc9d0cd6c7a3ca5ec92fd4ecb5
Zeyad Hasan <studying.zezo@gmail.com>
2025-10-23 21:37:42 +0300

    initial commit

---
+++ src/main/java/com/mycompany/studentmanagementsystem/StudentManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentmanagementsystem;
+
+/**
+ *
+ * @author Mega
+ */
+public class StudentManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
