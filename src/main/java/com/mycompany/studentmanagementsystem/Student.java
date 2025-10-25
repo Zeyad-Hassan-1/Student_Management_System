bd198ff72f2a489fbe05042142f6be280c9cf0e4
Hazem Essam <hazem3ssam20@gmail.com>
2025-10-26 02:18:46 +0300

    1- Finished Student Database class with all of the following operations: Public Methods: public String getFileName()
    public final void readFromFile() throws FileNotFoundException, NullPointerException, InputMismatchException
    
    public Student createStudentFrom(String line) throws NumberFormatException, InputMismatchException
    
    public void saveToFile() throws Exception
    
    public ArrayList<Student> returnAllStudents()
    
    public Student searchStudent(int studentId)
    
    public ArrayList<Student> searchStudent(String fullName)
    
    public void addStudent(int studentId, String fullName, int age, int GENDER_OPTION, String department, double GPA) throws InputMismatchException
    
    public void addStudent(String fullName, int age, int GENDER_OPTION, String department, double GPA) throws InputMismatchException
    
    public void addStudent(Student student) throws InputMismatchException
    
    public boolean deleteStudentById(int studentId)
    
    public void editStudentById(int studentId, int age, String department, double GPA) throws InputMismatchException
    
    public void editStudentById(int studentId, int age) throws InputMismatchException
    
    public void editStudentById(int studentId, double GPA) throws InputMismatchException
    
    public void editStudentById(int studentId, String department) throws InputMismatchException
    
    Private Methods:
    private int getMaxId()
    
    2- made minor modifications on student class like formatting and removing some setters for better logic flow

--- src/main/java/com/mycompany/studentmanagementsystem/Student.java
+++ src/main/java/com/mycompany/studentmanagementsystem/Student.java
@@ -11,61 +11,54 @@ import java.util.InputMismatchException;
  * @author Hazem
  */
 public class Student {
+
     public static final int MALE_OPTION = 1;
     public static final int FEMALE_OPTION = 0;
-    
-    
-    private int studentID = 0;
+
+    private int studentId = 0;
     private String fullName;
     private int age = 0;
     private int gender;
     private String department;
-    private double GPA =0.00;
+    private double GPA = 0.00;
 
-    public Student(int studentID, String fullName, int age, int GENDER_OPTION, String department, double GPA) throws InputMismatchException {
+    /**
+     *
+     * @param studentId
+     * @param fullName
+     * @param age
+     * @param GENDER_OPTION
+     * @param department
+     * @param GPA
+     * @throws InputMismatchException
+     */
+    public Student(int studentId, String fullName, int age, int GENDER_OPTION, String department, double GPA) throws InputMismatchException {
         this.fullName = fullName;
         this.department = department;
-        
-        
-        if(studentID<=0)
-        {
-            throw new InputMismatchException("Invalid Student ID: Please enter positive integer"); 
+
+        if (studentId <= 0) {
+            throw new InputMismatchException("Invalid Student Id: Please enter positive integer");
         }
-        if(age<=0)
-        {
+        if (age <= 0) {
             throw new InputMismatchException("Invalid Age: Please enter positive integer");
         }
-        if(GENDER_OPTION!=MALE_OPTION&&GENDER_OPTION!=FEMALE_OPTION)
-        {
+        if (GENDER_OPTION != MALE_OPTION && GENDER_OPTION != FEMALE_OPTION) {
             throw new InputMismatchException("Invalid Gender: Please enter either Student.MALE_OPTION or Student.FEMALE_OPTION");
         }
-        if(GPA<0.00f)
-        {
+        if (GPA < 0.00f) {
             throw new InputMismatchException("Invalid GPA: Please enter non-negative decimal number");
         }
-        this.studentID = studentID;
+        this.studentId = studentId;
         this.age = age;
         this.gender = GENDER_OPTION;
         this.GPA = GPA;
     }
 
     /**
-     * @return the studentID
+     * @return the studentId
      */
-    public int getStudentID() {
-        return studentID;
-    }
-
-    /**
-     * @param studentID the studentID to set
-     */
-    public void setStudentID(int studentID) throws InputMismatchException{
-        if(studentID<=0)
-        {
-            throw new InputMismatchException("Invalid Student ID: Please enter positive integer"); 
-            
-        }
-        this.studentID = studentID;
+    public int getStudentId() {
+        return studentId;
     }
 
     /**
@@ -73,13 +66,6 @@ public class Student {
      */
     public String getFullName() {
         return fullName;
-    }
-
-    /**
-     * @param fullName the fullName to set
-     */
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
     }
 
     /**
@@ -93,8 +79,7 @@ public class Student {
      * @param age the age to set
      */
     public void setAge(int age) throws InputMismatchException {
-        if(age<=0)
-        {
+        if (age <= 0) {
             throw new InputMismatchException("Invalid Age: Please enter positive integer");
         }
         this.age = age;
@@ -105,17 +90,6 @@ public class Student {
      */
     public int getGender() {
         return gender;
-    }
-
-    /**
-     * @param GENDER_OPTION either Student.MALE_OPTION or Student.FEMALE_OPTION to set
-     */
-    public void setGender(int GENDER_OPTION)throws InputMismatchException {
-        if(GENDER_OPTION!=MALE_OPTION&&GENDER_OPTION!=FEMALE_OPTION)
-        {
-            throw new InputMismatchException("Invalid Gender: Please enter either Student.MALE_OPTION or Student.FEMALE_OPTION");
-        }
-        this.gender = GENDER_OPTION;
     }
 
     /**
@@ -142,17 +116,15 @@ public class Student {
     /**
      * @param GPA the GPA to set
      */
-    public void setGPA(float GPA) throws InputMismatchException {
-        if(GPA<0.00f)
-        {
+    public void setGPA(double GPA) throws InputMismatchException {
+        if (GPA < 0.00f) {
             throw new InputMismatchException("Invalid GPA: Please enter non-negative decimal number");
         }
         this.GPA = GPA;
     }
-    
-    public String lineRepresentation()
-    {
-        return studentID + "," + fullName + "," + age + "," + ((gender==MALE_OPTION)?"male":"female")
+
+    public String lineRepresentation() {
+        return studentId + "," + fullName + "," + age + "," + ((gender == MALE_OPTION) ? "male" : "female")
                 + "," + department + "," + GPA;
     }
 }
--- src/main/java/com/mycompany/studentmanagementsystem/database/StudentDatabase.java
+++ src/main/java/com/mycompany/studentmanagementsystem/database/StudentDatabase.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -16,40 +17,58 @@ import java.util.Scanner;
  * @author Hazem
  */
 public class StudentDatabase {
+
     private String fileName;
     private ArrayList<Student> database;
 
     /**
-     * 
+     *
      * @param fileName
      * @throws FileNotFoundException if couldn't find a file with the given name
-     * @throws NullPointerException if couldn't create a Student instance due to invalid line format in the file 
+     * @throws NullPointerException if couldn't create a Student instance due to
+     * invalid line format in the file
      */
-    public StudentDatabase(String fileName) throws FileNotFoundException, NullPointerException
-    {
-        
+    public StudentDatabase(String fileName) throws FileNotFoundException, NullPointerException {
+
         this.fileName = fileName;
         database = new ArrayList<>();
-        try
-        {
+        try {
             this.readFromFile();
-        }
-        catch(FileNotFoundException | NullPointerException e)
-        {
+        } catch (FileNotFoundException | NullPointerException e) {
             throw e;
         }
     }
-    
+
     public String getFileName() {
         return fileName;
     }
+
+    private int getMaxId()
+    {
+        if(database.isEmpty())
+            return 0;
+        
+        int max = database.get(0).getStudentId();
+        for(Student student:database)
+        {
+            int currentId = student.getStudentId();
+            if(currentId>max)
+            {
+                max = currentId;
+            }
+        }
+        return max;
+    }
     
     /**
+     * THIS METHOD MUST BE CALLED BEFORE EVERY LOADING ANY PANEL or DOING MULTIPLE OPERATIONS IN SAME PANEL ON DATABASE
      * 
      * @throws FileNotFoundException if couldn't find a file with the given name
-     * @throws NullPointerException if couldn't create a Student instance due to invalid line format in the file
+     * @throws NullPointerException if couldn't create a Student instance due to
+     *         invalid line format in the file
+     * @throws InputMismatchException 
      */
-    public final void  readFromFile() throws FileNotFoundException,NullPointerException {
+    public final void readFromFile() throws FileNotFoundException, NullPointerException, InputMismatchException {
         File file = new File(this.fileName);
 
         try (Scanner scanner = new Scanner(file)) {
@@ -58,54 +77,285 @@ public class StudentDatabase {
                 Student student = createStudentFrom(scanner.nextLine());
                 if (student != null) {
                     database.add(student);
-                }
-                else
-                {
-                    throw new NullPointerException("Couldn't read from file:"+this.fileName +", Invalid line format");
+                } else {
+                    throw new NullPointerException("Couldn't read from file:" + this.fileName + ", Invalid line format");
                 }
             }
         } catch (FileNotFoundException e) {
             throw new FileNotFoundException("File not found: " + this.fileName);
         }
+        catch (InputMismatchException e)
+        {
+            throw e;
+        }
     }
-    
-    public Student createStudentFrom(String line) {
-        if(line == null){
+
+    /**
+     *  
+     *
+     * Note that!! 
+     * field[0] = (String)studentId; --> to be casted to int
+     * field[1] = (String)fullName;
+     * field[2] = (String)age; --> to be casted to int
+     * field[3] = (String)gender; --> to be casted to int 
+     * field[4] = (String)department; 
+     * field[5] = (String)GPA; --> to be casted to double
+     * Total are 6 fields
+     * 
+     * @param line
+     * @return a Student object from a comma-separated line of text or null if the line is invalid or cannot be parsed 
+     * @throws InputMismatchException
+     * @throws NumberFormatException
+     */
+    public Student createStudentFrom(String line) throws NumberFormatException,InputMismatchException {
+        if (line == null) {
             return null;
         }
-        String[] fields = line.split(","); 
-        /**
-         * Note that!!
-         * field[0] = (String)studentID; --> to be casted to int
-         * field[1] = (String)fullName; 
-         * field[2] = (String)age;       --> to be casted to int
-         * field[3] = (String)gender;    --> to be casted to int
-         * field[4] = (String)department;
-         * field[5] = (String)GPA;       --> to be casted to double
-         * Total are 6 fields
-         */
-        if(fields.length != 6){
-            return  null;
-        }
-        try
-        {
-           return  new Student(Integer.parseInt(fields[0]),fields[1],Integer.parseInt(fields[2]),Integer.parseInt(fields[3]),fields[4],Double.parseDouble(fields[5])); 
-        }
-        catch(NumberFormatException e)
-        {
+        String[] fields = line.split(",");
+        if (fields.length != 6) {
             return null;
         }
-        
+        try {
+            return new Student(Integer.parseInt(fields[0]), fields[1], Integer.parseInt(fields[2]), Integer.parseInt(fields[3]), fields[4], Double.parseDouble(fields[5]));
+        } catch (NumberFormatException | InputMismatchException e) {
+            throw e;
+        }
     }
-    
+
+    /**
+     * THIS METHOD MUST BE CALLED BEFORE EXITING ANY OPERATION ON THE DATABASE
+     *
+     * @throws Exception in case of save to file error
+     */
     public void saveToFile() throws Exception {
         File file = new File(this.fileName);
         try (FileWriter writer = new FileWriter(file)) {
-            for (Student student: this.database) {
+            for (Student student : this.database) {
                 writer.write(student.lineRepresentation() + "\n");
             }
         } catch (Exception e) {
-            throw new Exception("Error saving to file: " +  this.fileName);
+            throw new Exception("Error saving to file: " + this.fileName);
         }
     }
+
+    /**
+     * 
+     * @return ArrayList<Student> representing the database
+     */
+    public ArrayList<Student> returnAllStudents() {
+        return this.database;
+    }
+
+    /**
+    * Searches for a student in the database by their unique student ID.
+    *
+    * @param studentId the unique identifier of the student to search for
+    * @return Student object with the specified ID, or null if not found
+    */
+    public Student searchStudent(int studentId) {
+        if(this.database.isEmpty())
+            return null;
+        
+        for (Student student : this.database) {
+            if (student.getStudentId() == studentId) {
+                return student;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * 
+     * @param fullName
+     * @return Arraylist<Student> targets which has a list of all names that start with the argument fullName
+     *         or empty list in case of no matches
+     */
+    public ArrayList<Student> searchStudent(String fullName)
+    {
+        if(this.database.isEmpty())
+            return null;
+        
+        ArrayList<Student> targets = new ArrayList<Student>();
+        for(Student student : this.database)
+        {
+            if(student.getFullName().startsWith(fullName));
+            targets.add(student);
+        }
+        return targets;
+    }
+    
+    /**
+     * 
+     * @param studentId
+     * @param fullName
+     * @param age
+     * @param GENDER_OPTION
+     * @param department
+     * @param GPA
+     * @throws InputMismatchException in case of any violation to standard Student parameters format or pre-existing ID
+     */
+    public void addStudent(int studentId, String fullName, int age, int GENDER_OPTION, String department, double GPA) throws InputMismatchException
+    {
+        try
+        {
+           Student student = new Student(studentId,fullName,age,GENDER_OPTION,department,GPA);
+            addStudent(student);
+        }
+        catch(InputMismatchException e)
+        {
+            throw e;
+        }
+    }
+    
+    /**
+     * 
+     * @param fullName
+     * @param age
+     * @param GENDER_OPTION
+     * @param department
+     * @param GPA
+     * @throws InputMismatchException in case of any violation to standard Student parameters format
+     */
+    public void addStudent(String fullName, int age, int GENDER_OPTION, String department, double GPA) throws InputMismatchException
+    {
+        try
+        {
+           addStudent(getMaxId()+1,fullName,age,GENDER_OPTION,department,GPA);
+        }
+        catch(InputMismatchException e)
+        {
+            throw e;
+        }
+    }
+    
+    /**
+     * 
+     * @param student
+     * @throws InputMismatchException in case of any violation to standard Student parameters format or pre-existing ID
+     */
+    public void addStudent(Student student) throws InputMismatchException
+    {
+        if(searchStudent(student.getStudentId())!=null)
+        {
+            throw new InputMismatchException("Error: "+student.getStudentId()+" Already Exists!");
+        }
+        database.add(student);
+    }
+    
+    /**
+     * 
+     * @param studentId
+     * @return true in case of successful deletion, false otherwise
+     */
+    public boolean deleteStudentById(int studentId)
+    {
+        Student target = searchStudent(studentId);
+        if(target==null)
+        {
+            return false;
+        }
+        
+        database.remove(target);
+        return true;
+    }
+    
+    /**
+     * 
+     * @param studentId
+     * @param fullName
+     * @param age
+     * @param GENDER_OPTION
+     * @param department
+     * @param GPA
+     * @throws InputMismatchException 
+     */
+    public void editStudentById(int studentId, int age, String department, double GPA) throws InputMismatchException
+    {
+        Student target = searchStudent(studentId);
+        if(target==null)
+        {
+            throw new InputMismatchException("Error: "+studentId+" Doesn't Exist!");
+        }
+        try
+        {
+            target.setAge(age);
+            target.setDepartment(department);
+            target.setGPA(GPA);
+        }
+        catch(InputMismatchException e)
+        {
+            throw e;
+        }
+    }
+    
+    /**
+     * 
+     * @param studentId
+     * @param age
+     * @throws InputMismatchException 
+     */
+    public void editStudentById(int studentId,int age) throws InputMismatchException
+    {
+      Student target = searchStudent(studentId);
+        if(target==null)
+        {
+            throw new InputMismatchException("Error: "+studentId+" Doesn't Exist!");
+        }
+        try
+        {
+            target.setAge(age);
+        }
+        catch(InputMismatchException e)
+        {
+            throw e;
+        }  
+    }
+    
+    /**
+     * 
+     * @param studentId
+     * @param GPA
+     * @throws InputMismatchException 
+     */
+    public void editStudentById(int studentId,double GPA) throws InputMismatchException
+    {
+      Student target = searchStudent(studentId);
+        if(target==null)
+        {
+            throw new InputMismatchException("Error: "+studentId+" Doesn't Exist!");
+        }
+        try
+        {
+            target.setGPA(GPA);
+        }
+        catch(InputMismatchException e)
+        {
+            throw e;
+        }  
+    }
+    
+    /**
+     * 
+     * @param studentId
+     * @param department
+     * @throws InputMismatchException 
+     */
+    public void editStudentById(int studentId,String department) throws InputMismatchException
+    {
+      Student target = searchStudent(studentId);
+        if(target==null)
+        {
+            throw new InputMismatchException("Error: "+studentId+" Doesn't Exist!");
+        }
+        try
+        {
+            target.setDepartment(department);
+        }
+        catch(InputMismatchException e)
+        {
+            throw e;
+        }  
+    }
+    
+    
 }
